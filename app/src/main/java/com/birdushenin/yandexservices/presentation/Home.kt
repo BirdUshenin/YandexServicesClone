2880c9510299cb5f77c296092f4b3f7af4e84b2d
Ilya <ilyaushenin@gmail.com>
2023-04-15 17:57:37 +0300

    Start of navigation production

---
+++ app/src/main/java/com/birdushenin/yandexservices/presentation/Home.kt
@@ -0,0 +1,12 @@
+package com.birdushenin.yandexservices.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.birdushenin.yandexservices.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/birdushenin/yandexservices/presentation/StartScreen.kt
@@ -0,0 +1,12 @@
+package com.birdushenin.yandexservices.presentation
+
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import com.birdushenin.yandexservices.R
+
+class StartScreen : Fragment (R.layout.activity_start_screen) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
