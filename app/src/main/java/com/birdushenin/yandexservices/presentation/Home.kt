6f7fcad573754e7449427f98b30174f8c9576667
Ilya <ilyaushenin@gmail.com>
2023-04-28 21:10:27 +0300

    Done navigation bottom + navJetpack

--- app/src/main/java/com/birdushenin/yandexservices/presentation/Home.kt
+++ app/src/main/java/com/birdushenin/yandexservices/presentation/Home.kt
@@ -1,21 +1,33 @@
 package com.birdushenin.yandexservices.presentation
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.birdushenin.yandexservices.R
 import com.birdushenin.yandexservices.databinding.FragmentHomeBinding
 
+
 class Home : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val binding = FragmentHomeBinding.inflate(layoutInflater)
-
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = (childFragmentManager.findFragmentById(R.id.fragment_container_view_tag) as NavHostFragment).navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
