21cc759acc0ade7e7875a9ee5e5d2987a28dd42e
Ilya <ilyaushenin@gmail.com>
2023-04-29 13:50:44 +0300

    Quarter

--- app/src/main/java/com/birdushenin/yandexservices/presentation/Search.kt
+++ app/src/main/java/com/birdushenin/yandexservices/presentation/Search.kt
@@ -1,60 +1,36 @@
 package com.birdushenin.yandexservices.presentation
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.birdushenin.yandexservices.R
+import com.birdushenin.yandexservices.databinding.FragmentHomeBinding
+import com.birdushenin.yandexservices.databinding.FragmentSearchBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Search.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Search : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        binding = FragmentSearchBinding.inflate(layoutInflater)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.adone)
+        binding.imageView7.setImageBitmap(bitmap)
+        val res = resources
+        val roundedDrawable = RoundedBitmapDrawableFactory.create(res, bitmap)
+        roundedDrawable.cornerRadius = 155.toFloat()
+        binding.imageView7.setImageDrawable(roundedDrawable)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Search.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Search().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
