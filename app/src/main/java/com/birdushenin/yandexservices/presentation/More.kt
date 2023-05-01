12db9dd45f6234da81e1e64ad0e3e9e73ca9c9a2
Ilya <ilyaushenin@gmail.com>
2023-05-02 00:40:47 +0300

    edit to check behavior

--- app/src/main/java/com/birdushenin/yandexservices/presentation/More.kt
+++ app/src/main/java/com/birdushenin/yandexservices/presentation/More.kt
@@ -7,10 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.birdushenin.yandexservices.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -18,17 +15,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class More : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,14 +34,6 @@ class More : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment More.
          */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            More().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
