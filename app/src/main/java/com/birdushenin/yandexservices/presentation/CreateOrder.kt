ad4a0d0c1fe8bf374d30b2eba921cbcbee081fd6
Ilya <ilyaushenin@gmail.com>
2023-05-03 17:17:51 +0300

    Cleaning up a project

--- app/src/main/java/com/birdushenin/yandexservices/presentation/CreateOrder.kt
+++ app/src/main/java/com/birdushenin/yandexservices/presentation/CreateOrder.kt
@@ -1,16 +1,14 @@
 package com.birdushenin.yandexservices.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.birdushenin.yandexservices.R
 import com.birdushenin.yandexservices.databinding.FragmentCreateOrderBinding
-import kotlinx.android.synthetic.main.fragment_create_order.*
-
 
 class CreateOrder : Fragment() {
 
