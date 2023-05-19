64134ae8e1c293c68a91356ca37ced562803b887
Ilya <ilyaushenin@gmail.com>
2023-05-19 19:45:34 +0300

    Cl-pr

--- app/src/main/java/com/birdushenin/yandexservices/presentation/Prompt.kt
+++ app/src/main/java/com/birdushenin/yandexservices/presentation/Prompt.kt
@@ -1,29 +1,19 @@
 package com.birdushenin.yandexservices.presentation
 
-import android.content.Context
-import android.content.res.Resources
-import android.graphics.Rect
 import android.os.Bundle
-import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewTreeObserver
-import android.widget.FrameLayout
 import android.widget.ImageView
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.core.view.marginTop
-import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.birdushenin.yandexservices.R
 import com.birdushenin.yandexservices.databinding.FragmentPromptBinding
 import com.birdushenin.yandexservices.presentation.adapter.RecyclerViewHorizontal
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import pl.droidsonroids.gif.GifDrawable
 
 class Prompt : Fragment() {
@@ -34,9 +24,6 @@ class Prompt : Fragment() {
     private lateinit var adapter: RecyclerViewHorizontal
 
     private lateinit var gifImageView: ImageView
-
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -64,10 +51,6 @@ class Prompt : Fragment() {
         gifImageView = binding.imageView34
 
         recyclerView = binding.recyclerView
-
-
-
-
 
         adapter = RecyclerViewHorizontal()
         recyclerView.layoutManager =
